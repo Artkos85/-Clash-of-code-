d0cbc62c4f81b2d87ed81b056dc28e09850aab8a
Артур Косяков <73811066+Artkos85@users.noreply.github.com>
2022-03-30 14:10:00 +0500

    Add files via upload

---
+++ Fact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+               
+                    /*You are given two numbers
+                    X and N
+                    You should output :
+                    X!/ (X - N)!
+                    Example:
+                    input: X = 6, N = 4
+                    output should be the result of 6*5*4*3*/
+
+                    int X = int.Parse(Console.ReadLine());
+                    int N = int.Parse(Console.ReadLine());
+                    
+                    // X!
+                    int fact1 = 1;
+                    for (int i = 1; i <= X; i++)
+                    {
+                        fact1 *= i;
+                    }
+
+                    //(X - N)!
+                    int fact2 = 1;
+                    for (int j = 1; j <= (X - N); j++)
+                    {
+                        fact2 *= j;
+                    }
+                   
+                    // You should output :  X! / (X - N)!
+                    int result = fact1 / fact2;
+                    Console.Write(result);
+                    Console.ReadLine();
+
+
+
+
+                
+
+
+
+
+        }
+    }
+}
