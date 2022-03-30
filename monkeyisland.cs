f25bb574cf23070887be30466d95edeb7bf19e81
Артур Косяков <73811066+Artkos85@users.noreply.github.com>
2022-03-30 12:58:44 +0500

    Add files via upload

---
+++ monkeyisland.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Monkeyisland
+{
+	public Monkeyisland()
+	{
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            float funkiness = float.Parse(Console.ReadLine());
+
+            string m = "monkey";
+
+            if (day == "friday")
+            {
+                m = "funky" + m + "fiday";
+            }
+            if (funkiness >= 7)
+            {
+                m = m.ToUpper();
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", m);
+            }
+            Console.ReadLine();
+            //console.log(m.repeat(n).trim());( in JS)
+        }
+    }
+}
